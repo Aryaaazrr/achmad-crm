87792cff1dd8be0e8b7b8fc2e6f6b15367e166b4
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2025-09-04 19:35:38 +0700

    [STYLE] styling create user page

--- resources/js/pages/users/create.tsx
+++ resources/js/pages/users/create.tsx
@@ -1,33 +1,43 @@
 import { AppSidebar } from '@/components/app-sidebar';
 import { SiteHeader } from '@/components/site-header';
 import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { router } from '@inertiajs/react';
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
+import { z } from 'zod';
 
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+const FormSchema = z
+    .object({
+        name: z.string().min(2, {
+            message: 'Name must be at least 2 characters.',
+        }),
+        email: z.string().email({
+            message: 'Invalid email address.',
+        }),
+        password: z.string().min(6, {
+            message: 'Password must be at least 6 characters.',
+        }),
+        confirmPassword: z.string().min(6, {
+            message: 'Confirm Password must be at least 6 characters.',
+        }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        path: ['confirmPassword'],
+        message: 'Passwords do not match.',
+    });
 
 export default function UsersCreate() {
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            username: '',
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
         },
     });
     function onSubmit(data: z.infer<typeof FormSchema>) {
@@ -56,15 +66,15 @@ export default function UsersCreate() {
                             <div className="mx-6 h-full rounded-xl bg-muted/50 p-4">
                                 <div className="flex items-center py-4">
                                     <Form {...form}>
-                                        <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 w-full gap-4 space-y-6">
+                                        <form onSubmit={form.handleSubmit(onSubmit)} className="grid w-full grid-cols-2 gap-4 space-y-6">
                                             <FormField
                                                 control={form.control}
-                                                name="username"
+                                                name="name"
                                                 render={({ field }) => (
                                                     <FormItem>
                                                         <FormLabel>Name</FormLabel>
                                                         <FormControl>
-                                                            <Input placeholder="shadcn" {...field} />
+                                                            <Input placeholder="Enter for name user" {...field} />
                                                         </FormControl>
                                                         <FormMessage />
                                                     </FormItem>
@@ -73,12 +83,12 @@ export default function UsersCreate() {
 
                                             <FormField
                                                 control={form.control}
-                                                name="username"
+                                                name="email"
                                                 render={({ field }) => (
                                                     <FormItem>
                                                         <FormLabel>Email</FormLabel>
                                                         <FormControl>
-                                                            <Input placeholder="shadcn" {...field} />
+                                                            <Input placeholder="Enter for email user" {...field} />
                                                         </FormControl>
                                                         <FormMessage />
                                                     </FormItem>
@@ -87,19 +97,37 @@ export default function UsersCreate() {
 
                                             <FormField
                                                 control={form.control}
-                                                name="username"
+                                                name="password"
                                                 render={({ field }) => (
                                                     <FormItem>
-                                                        <FormLabel>Email</FormLabel>
+                                                        <FormLabel>Password</FormLabel>
                                                         <FormControl>
-                                                            <Input placeholder="shadcn" {...field} />
+                                                            <Input type="password" placeholder="********" {...field} />
                                                         </FormControl>
                                                         <FormMessage />
                                                     </FormItem>
                                                 )}
                                             />
 
-                                            <Button type="submit">Submit</Button>
+                                            <FormField
+                                                control={form.control}
+                                                name="confirmPassword"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>Confirm Password</FormLabel>
+                                                        <FormControl>
+                                                            <Input type="password" placeholder="********" {...field} />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                            <div className="col-span-2 flex justify-center md:justify-end">
+                                                <Button className="w-full md:w-1/2" type="submit">
+                                                    Submit
+                                                </Button>
+                                            </div>
                                         </form>
                                     </Form>
                                 </div>
