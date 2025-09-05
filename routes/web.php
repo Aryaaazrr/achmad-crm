ec1854ef48de391e9e5157a45d86a21d3119ad94
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2025-09-05 21:18:01 +0700

    [FEAT] route : new route for feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\LeadsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,10 +19,55 @@ Route::middleware(['auth'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-    Route::delete('/users/bulk-destroy', [UserController::class, 'bulkDestroy'])
-    ->name('users.bulk-destroy');
-    Route::resource('users', UserController::class);
+    Route::middleware('role:'. User::ROLE_MANAGER)->group(function () {
+        Route::prefix('users')->name('users.')->group(function () {
+            Route::get('trash', [UserController::class, 'showDeleted'])->name('trash.index');
+            Route::delete('bulk-destroy', [UserController::class, 'bulkDestroy'])->name('bulk.destroy');
+            Route::post('{id}/restore', [UserController::class, 'restore'])->name('restore');
+            Route::delete('force-delete', [UserController::class, 'forceDelete'])->name('force-delete');
+        });
 
+        Route::resource('users', UserController::class)->except(['show']);
+
+        Route::prefix('product')->name('product.')->group(function () {
+            Route::get('/trash', [ProductController::class, 'showDeleted'])->name('trash.index');
+            Route::delete('/bulk-destroy', [ProductController::class, 'bulkDestroy'])->name('bulk-destroy');
+            Route::post('/{id}/restore', [ProductController::class, 'restore'])->name('restore');
+            Route::delete('force-delete', [ProductController::class, 'forceDelete'])->name('force-delete');
+        });
+
+        Route::resource('product', ProductController::class);
+    });
+
+    Route::prefix('leads')->name('leads.')->group(function () {
+        Route::get('/', [LeadsController::class, 'index'])->name('index');
+        Route::get('/{id}/edit', [LeadsController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [LeadsController::class, 'update'])->name('update');
+
+        Route::middleware('role:' . User::ROLE_SALES)->group(function () {
+            Route::get('/create', [LeadsController::class, 'create'])->name('create');
+            Route::post('/', [LeadsController::class, 'store'])->name('store');
+            Route::delete('/', [LeadsController::class, 'bulkDestroy'])->name('bulk-destroy');
+            Route::delete('/{id}', [LeadsController::class, 'destroy'])->name('destroy');
+        });
+    });
+
+    Route::prefix('project')->name('project.')->group(function () {
+        Route::get('/', [ProjectController::class, 'index'])->name('index');
+        Route::get('/{id}', [ProjectController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [ProjectController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [ProjectController::class, 'update'])->name('update');
+
+        Route::middleware('role:' . User::ROLE_SALES)->group(function () {
+            Route::get('/create', [ProjectController::class, 'create'])->name('create');
+            Route::post('/', [ProjectController::class, 'store'])->name('store');
+            Route::delete('/', [ProjectController::class, 'bulkDestroy'])->name('bulk-destroy');
+            Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('destroy');
+        });
+    });
+
+    Route::resource('customer', CustomerController::class);
+    Route::resource('report', ReportController::class);
 });
 
 require __DIR__.'/settings.php';
